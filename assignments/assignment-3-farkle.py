6b782067fce87b2ac2354edb9230fb6b49715c15
Pollot <79442640+Pollot@users.noreply.github.com>
2026-05-13 12:31:00 +0200

    Created farkle scoring logic

--- assignments/assignment-3-farkle.py
+++ assignments/assignment-3-farkle.py
@@ -1,6 +1,6 @@
-WINNING_SCORE = 2000 # winning score
-cur_player = 0 # index of the current player
-winner = '' # name of the winner
+WINNING_SCORE = 2000  # winning score
+cur_player = 0  # index of the current player
+winner = ''  # name of the winner
 
 print("Let's play farkle!")
 
@@ -13,7 +13,7 @@ while True:
 
 scores = [0] * num_players
 
-def player_names(num_players):
+def player_names(num_players: int) -> list:
     players = []
 
     print("Enter a name for each player.")
@@ -36,3 +36,45 @@ while True:
         break
     else:
         print("Answer yes/no (y/n)!")
+
+def calc_score(throw: list[int]) -> int:
+    score = 0
+
+    if sorted(throw) == [1, 2, 3, 4, 5, 6]:
+        score = 2500
+        return score
+    
+    count = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0
+    }
+
+    for dice in throw:
+        count[dice] += 1
+
+    for face in range(1, 7):
+        if face == 1:
+            base_score = 1000
+        else:
+            base_score = face * 100
+
+        if count[face] == 6:
+            score += base_score * 4
+        elif count[face] == 5:
+            score += base_score * 3
+        elif count[face] == 4:
+            score += base_score * 2
+        elif count[face] == 3:
+            score += base_score
+        elif face == 1:
+            score += count[face] * 100
+        elif face == 5:
+            score += count[face] * 50
+
+    return score
+
+# print(calc_score([1, 2, 3, 4, 5, 6]))  # Testing
